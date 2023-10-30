631b7bf3794b589a652d8e4f258ea9f70c84ce01
Grzegorz <grzegorz.gejlo@cyfroweszkoly.pl>
2023-10-30 09:36:11 +0100

    polishking

---
+++ projket/App.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native';
+import PolishKing from './polsihKing';
+
+export default function App() {
+  return (
+    <View>
+      <PolishKing />
+    </View>
+  )
+}
+
---
+++ projket/polsihKing.js
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native' ;
+
+const polishKing = () =>{
+    return(
+        <View>
+            <Text>Polish King is comming</Text>
+        </View>
+    )
+}
+
+export default polishKing;
